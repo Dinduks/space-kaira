0498cd0ab5e78f84c7e2cb6e5b7dc4563a3da2ca
stokayo <stokayo>
2013-12-22 16:42:37 +0100

    add planet generator

---
+++ src/fr/upem/spacekaira/shape/character/Iterators.java
@@ -0,0 +1,34 @@
+package fr.upem.spacekaira.shape.character;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Iterators {
+
+     public static <E> Iterator<E> asIterator(List<List<E>> lists) {
+         List<Iterator<E>> iterators = new ArrayList<>(lists.size());
+         for(List<E> list : lists) {
+             if(list != null)
+                iterators.add(list.iterator());
+         }
+
+         return new Iterator<E> () {
+            int current=0;
+             public boolean hasNext() {
+                 while ( current < iterators.size() && !iterators.get(current).hasNext() )
+                     current++;
+
+                 return current < iterators.size();
+             }
+
+             public E next() {
+                 while ( current < iterators.size() && !iterators.get(current).hasNext() )
+                     current++;
+
+                 return iterators.get(current).next();
+             }
+         };
+    }
+}
---
+++ src/fr/upem/spacekaira/shape/character/PlanetGenerator.java
@@ -0,0 +1,7 @@
+package fr.upem.spacekaira.shape.character;
+
+/**
+ * Created by stokayo on 21/12/13.
+ */
+public class PlanetGenerator {
+}
