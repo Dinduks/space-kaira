1c773bf5106bba866e24d7f7ec51a1d197f1fe56
Samy Dindane <samy@dindane.com>
2013-12-29 13:22:15 +0000

    Add configuration validation

--- src/fr/upem/spacekaira/config/ConfigurationLoader.java
+++ src/fr/upem/spacekaira/config/ConfigurationLoader.java
@@ -1,25 +1,51 @@
 package fr.upem.spacekaira.config;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.bind.*;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 import java.io.File;
 
 public final class ConfigurationLoader {
     private ConfigurationLoader() {}
 
+    /**
+     * Builds a configuration from a level description file.
+     *
+     * @param file The level description file
+     * @return     The build Configuration
+     * @throws     ConfigurationParsingException
+     */
     public static Configuration loadFrom(File file)
             throws ConfigurationParsingException {
         try {
-            JAXBContext jaxbContext = JAXBContext.newInstance(Configuration.class);
+            JAXBContext context = JAXBContext.newInstance(Configuration.class);
 
-            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            Configuration configuration =
-                    (Configuration) jaxbUnmarshaller.unmarshal(file);
+            SchemaFactory sf = SchemaFactory.newInstance(
+                    XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = sf.newSchema(new File("level.xsd"));
 
-            return configuration;
-        } catch (JAXBException e) {
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            unmarshaller.setSchema(schema);
+            unmarshaller.setEventHandler(new ConfigurationEventHandler());
+            Configuration config = (Configuration) unmarshaller.unmarshal(file);
+
+            return config;
+        } catch (Exception e) {
             throw new ConfigurationParsingException(e.toString());
+        }
+    }
+
+    /**
+     * This event handler makes the marshalling crash if there's any error or
+     * warning
+     */
+    public static class ConfigurationEventHandler implements ValidationEventHandler {
+        @Override
+        public boolean handleEvent(ValidationEvent event) {
+            return false;
         }
     }
 }
